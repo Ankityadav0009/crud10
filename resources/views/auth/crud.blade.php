4f7a35e68bd7ad1f9169ecea9d4f93a551f681c8
Ankit <ankitdv759@gmail.com>
2023-05-18 08:10:29 +0530

    a2

--- resources/views/auth/crud.blade.php
+++ resources/views/auth/crud.blade.php
@@ -8,16 +8,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     
 </head>
-<body>
-     <a href="add">Add</a> 
-       
-     <table class="table">
+<body >
+       <br>
+      <a href="add"><button class='btn btn-primary'>Add User</button></a>
+     <table class="table" style=" border:1px;">
   <thead>
     <tr>
       <th scope="col">Id</th>
       <th scope="col">Name</th>
       <th scope="col">Age</th>
       <th scope="col">City</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -27,9 +28,8 @@
       <td>{{$item['name']}}</td>
       <td>{{$item['age']}}</td>
       <td>{{$item['city']}}</td>
-      
-      <td><a href="{{url('delete')}}/{{$item['id']}}"><button class='btn btn-primary'>Delete</button></a></td>
-      <td><a href="{{route('user.update',['id'=>$item->id])}}"><button class='btn btn-primary'>Update</button></a></td>
+      <td><a href="{{url('delete')}}/{{$item['id']}}"><button class='btn btn-primary'>Delete</button></a>
+      <a href="{{route('user.update',['id'=>$item->id])}}"><button class='btn btn-primary'>Update</button></a></td>
     
       @endforeach
     </tr>
